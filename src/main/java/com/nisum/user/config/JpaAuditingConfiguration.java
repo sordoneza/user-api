4f869e2b317c17c9f199660def76a406292ddce4
sordoneza <sordonez88@gmail.com>
2022-05-28 22:57:45 -0600

    Enable Jpa Auditing

---
+++ src/main/java/com/nisum/user/config/JpaAuditingConfiguration.java
@@ -0,0 +1,11 @@
+package com.nisum.user.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@Configuration
+@EnableJpaAuditing()
+class JpaAuditingConfiguration {
+
+}
