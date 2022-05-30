c10db5ee60753bc243bee44cce1170a01944c68a
sordoneza <sordonez88@gmail.com>
2022-05-29 18:22:58 -0600

    Include maven wrapper

--- src/main/java/com/nisum/user/UserApplication.java
+++ src/main/java/com/nisum/user/UserApplication.java
@@ -1,9 +1,17 @@
 package com.nisum.user;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "User API", version = "1.0", description = "OpenApi specification with user endpoints"))
+@SecurityScheme(name = "JWT", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class UserApplication {
 
     public static void main(String[] args) {
