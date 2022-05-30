2f95107a84bdf6e0e662ba0a72e7aed98ee03a41
sordoneza <sordonez88@gmail.com>
2022-05-29 18:43:08 -0600

    Config Swagger annotations

--- src/main/java/com/nisum/user/UserApplication.java
+++ src/main/java/com/nisum/user/UserApplication.java
@@ -3,6 +3,7 @@ package com.nisum.user;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
@@ -10,8 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "User API", version = "1.0", description = "OpenApi specification with user endpoints"))
-@SecurityScheme(name = "JWT", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(info = @Info(title = "User API", version = "1.0", description = "OpenAPI specification for user endpoints",
+        contact = @Contact(name = "Sergio Ordoñez", email = "sordonez88@gmail.com")))
+@SecurityScheme(name = "JWT", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER, bearerFormat = "JWT")
 public class UserApplication {
 
     public static void main(String[] args) {
