40f9b7ead5ed045865575100fab812fba080383f
sordoneza <sordonez88@gmail.com>
2022-05-30 00:01:21 -0600

    Add No Argument constructor

--- src/main/java/com/nisum/user/model/dto/UserInput.java
+++ src/main/java/com/nisum/user/model/dto/UserInput.java
@@ -2,6 +2,7 @@ package com.nisum.user.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.validation.constraints.Email;
@@ -10,6 +11,7 @@ import javax.validation.constraints.NotNull;
 import java.util.Collection;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserInput {
 
